35cc338453ac86ead9a4ae2ef1dc0d24e1dcb1de
pavanleton <pavankumarkpkp@gmail.com>
2023-12-02 20:42:35 +0530

    throw error if mongo URI is missing

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose"
 
 export const connectToDB = async () => {
-    // return mongoose.connect(process.env.MONGO_URI);
-    return new Promise((resolve, reject) => {
-        return resolve(true)
-    })
+    if(!process.env.MONGO_URI) throw new Error("Missing MONGO_URI environment variable")
+    if (mongoose.connection.readyState >= 1) return;
+    return mongoose.connect(process.env.MONGO_URI);
 }
