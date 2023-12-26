cc8a05a70805db2b4bbb4aaa107454e0b2090814
Sandeep <sandbro7163337@gmail.com>
2023-12-26 18:52:42 +0530

    feat(User Service): add `findUserByEmail`

--- services/user/index.js
+++ services/user/index.js
@@ -1,9 +1,32 @@
-export const findUserByEmail = async (email) => {}
+import { userModel } from "../../db/models/user.js";
+import { ApiError } from "../../utils/ApiError.js";
 
-export const createUser = async (user) => {}
+/**
+ *
+ * @param {string} email - to unique identifier of the user
+ * @returns {Promise<userModel>} - to resolve to the find user
+ * @throws {Error} - if error occured in finding user.
+ */
 
-export const findUserById = async (id) => {}
+export const findUserByEmail = async (email) => {
+  try {
+    if (!email) throw new ApiError(400, "Email is required");
 
-export const updateUserById = async (id, propertiesToBeUpdated) => {}
+    const user = await userModel.findOne({
+      email,
+    });
+
+    if (!user) throw new ApiError(404, "User doesn't exist");
+    return user;
+  } catch (error) {
+    throw new ApiError(500, "Somethings went wrong in finding user");
+  }
+};
+
+export const createUser = async (user) => {};
+
+export const findUserById = async (id) => {};
+
+export const updateUserById = async (id, propertiesToBeUpdated) => {};
 
 // more, if required
