305bb8c3efc132b3ed44b8d0754ef272547de72a
Sandeep <sandbro7163337@gmail.com>
2024-01-02 17:09:58 +0530

    feat/(user model)/remove methods

--- db/models/user.js
+++ db/models/user.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+ 
 
 const schema = new mongoose.Schema(
   {
@@ -21,28 +21,8 @@ const schema = new mongoose.Schema(
   }
 );
 
-/**
- *
- * @param {string} password - to check the user password
- * @returns {Promise<boolean>} - promise to resolve true or false
- */
+ 
 
-schema.methods.isPasswordCorrect = async (password) => {
-  return await bcrypt.compare(password, this.password);
-};
-
-/**
- *
- *
- * @returns {Promise<string>} - promise that resolve to the generated access token (jwt)
- */
-schema.methods.generateAccessToken = () => {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.ACCESS_TOKEN_SECRET
-  );
-};
+ 
 
 export const userModel = mongoose.model("User", schema);
