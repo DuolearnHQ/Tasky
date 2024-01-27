14f56373e1258ef99ee37b17893a76e91bd12ee3
Sandeep <sandbro7163337@gmail.com>
2024-01-27 14:16:50 +0530

    feat/controller(auth)/ add `login`

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { ApiError } from "../../utils/ApiError.js";
 import { findUserByEmail } from "../../services/user/index.js";
-import generateToken from "../../services/auth/index.js";
 import bcrpyt from "bcrypt";
 import { createUser } from "../../services/user/index.js";
 import { createToken } from "../../services/jwt/index.js";
@@ -17,13 +16,7 @@ const registerBodyPayload = z.object({
     password: z.string().min(8, "Password must be at least 8 characters long"),
 });
 
-router.post("/login", (req, res) => {
-    return res
-        .json({
-            access_token: "",
-        })
-        .status(200);
-});
+
 
 /**
  * @swagger
@@ -103,5 +96,96 @@ router.post("/register", async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 });
+
+
+
+
+/**
+ * @swagger
+ * swagger: "2.0"
+ * tags:
+ *   - name: "Users"
+ *     description: "User management and login"
+ * paths:
+ *   /auth/login:
+ *     post:
+ *       summary: "Login user"
+ *       description: "Login a  user in the system."
+ *       tags:
+ *         - "Users"
+ *       consumes:
+ *         - application/json
+ *       parameters:
+ *         - in: body
+ *           name: user
+ *           description: "the user to create"
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: "string"
+ *                 description: "The user's email address"
+ *               password:
+ *                 type: "string"
+ *                 description: "The user's password"
+ *       responses:
+ *         200:
+ *           description: "Login successfully"
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: "object"
+ *                 properties:
+ *                   token:
+ *                     type: "string"
+ *                     description: "Authentication token for the user"
+ *         400:
+ *           description: "Invalid input"
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: "object"
+ *                 properties:
+ *                   message:
+ *                      type: "string"
+ *                      description: "Error message detailing what went wrong"
+ *         500:
+ *            description: "Internal server error"
+ */
+ 
+
+router.post("/login", async (req, res) => {
+  try {
+   
+    const { email, password } = req.body;
+
+    if (!email) throw new ApiError(400, "Email is required");
+
+    const user = await findUserByEmail(email);
+ 
+    if (!user) throw new ApiError(404, "User doesn't exist");
+
+    const isPasswordValid = await bcrpyt.compare(password, user?.password);
+    if (!isPasswordValid) throw new ApiError(401, "Invalid user credentials");
+
+    //generate access Token
+    const accessToken = await createToken(user?._id);
+ 
+
+     return res
+      .json({
+        access_token: accessToken,
+      })
+      .status(200);
+  } catch (error) {
+ 
+    throw new ApiError(400, "ERRR in login");
+  }
+});
+
+
 
 export default router;
